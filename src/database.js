f05576558cd29a14a7bcc55922e963b70d4b726e
Luis Gabriel <janco7249@gmail.com>
2021-10-11 10:14:00 -0400

    database correct

--- src/database.js
+++ src/database.js
@@ -3,11 +3,19 @@ const mongoose = require('mongoose')
 const { MONGO_DB_URI, MONGO_DB_URI_TEST, NODE_ENV } = process.env
 // le decimos que cree una base datos mongodb
 const connectionString = NODE_ENV === 'test' ? MONGO_DB_URI_TEST : MONGO_DB_URI
-
+console.log(connectionString)
 // cuando la coneccion sea abierta ejecute algo por consola
-mongoose.connect(connectionString).then(() => {
-  console.log('DB is connected')
-}).catch(err => console.log(err))
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+  .then(() => {
+    console.log('Database connected')
+  }).catch(err => {
+    console.error(err)
+  })
 
 // por si se rompe algo quitamos la coneccion para que no quede zombie
 process.on('uncaughtException', (err) => {
