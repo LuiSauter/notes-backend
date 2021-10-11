45b2ff73cc4d94c2b5fc094de072af66c7537b52
Luis Gabriel <janco7249@gmail.com>
2021-10-11 09:13:49 -0400

    mongodb config

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 // Codigo de la coneccion a la base de datos
 const mongoose = require('mongoose')
-const { MONGO_DB_URI, MONGO_DB_URI_TEST } = process.env
+const { MONGO_DB_URI, MONGO_DB_URI_TEST, NODE_ENV } = process.env
 // le decimos que cree una base datos mongodb
-const connectionString = MONGO_DB_URI || MONGO_DB_URI_TEST
+const connectionString = NODE_ENV === 'test' ? MONGO_DB_URI_TEST : MONGO_DB_URI
 
 // cuando la coneccion sea abierta ejecute algo por consola
 mongoose.connect(connectionString).then(() => {
@@ -10,6 +10,7 @@ mongoose.connect(connectionString).then(() => {
 }).catch(err => console.log(err))
 
 // por si se rompe algo quitamos la coneccion para que no quede zombie
-process.on('uncaughtException', () => {
+process.on('uncaughtException', (err) => {
+  console.log(err)
   mongoose.connection.disconnect()
 })
